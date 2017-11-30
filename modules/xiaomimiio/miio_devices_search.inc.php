e90bd0e61b0521aa0e9654ff37499550abdd015b
skysilver <skysilver.da@gmail.com>
2017-11-30 16:37:19 +0300

    v.0.2-alpha

--- modules/xiaomimiio/miio_devices_search.inc.php
+++ modules/xiaomimiio/miio_devices_search.inc.php
@@ -2,29 +2,36 @@
 /*
 * @version 0.1 (wizard)
 */
- global $session;
-  if ($this->owner->name=='panel') {
-   $out['CONTROLPANEL']=1;
-  }
-  $qry="1";
-  // search filters
-  // QUERY READY
-  global $save_qry;
-  if ($save_qry) {
-   $qry=$session->data['miio_devices_qry'];
-  } else {
-   $session->data['miio_devices_qry']=$qry;
-  }
-  if (!$qry) $qry="1";
-  $sortby_miio_devices="ID DESC";
-  $out['SORTBY']=$sortby_miio_devices;
-  // SEARCH RESULTS
-  $res=SQLSelect("SELECT * FROM miio_devices WHERE $qry ORDER BY ".$sortby_miio_devices);
-  if ($res[0]['ID']) {
-   //paging($res, 100, $out); // search result paging
-   $total=count($res);
-   for($i=0;$i<$total;$i++) {
-    // some action for every record if required
-   }
-   $out['RESULT']=$res;
-  }
+
+global $session;
+
+if ($this->owner->name == 'panel') {
+	$out['CONTROLPANEL'] = 1;
+}
+
+$qry = '1';
+
+global $save_qry;
+
+if ($save_qry) {
+	$qry = $session->data['miio_devices_qry'];
+} else {
+	$session->data['miio_devices_qry'] = $qry;
+}
+
+if (!$qry) $qry = '1';
+
+$sortby_miio_devices = 'ID DESC';
+
+$out['SORTBY'] = $sortby_miio_devices;
+  
+$res = SQLSelect("SELECT * FROM miio_devices WHERE $qry ORDER BY " . $sortby_miio_devices);
+
+if ($res[0]['ID']) {
+	//paging($res, 100, $out); // search result paging
+	$total = count($res);
+	for($i = 0; $i < $total; $i++) {
+		// some action for every record if required
+	}
+	$out['RESULT'] = $res;
+}
